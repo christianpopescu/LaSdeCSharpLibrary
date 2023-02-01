c0f83bfb8a09735e1790bfda44da42b1e26acce0
Christian Popescu <christian.popescu@outlook.com>
2023-02-01 23:38:48 +0100

    Add File Repository

---
+++ src/LaSdeCSharpLibrary/LaSdeCSharpLibrary/FileRepository/FileRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaSdeCSharpLibrary.FileRepository
+{
+    internal class FileRepository
+    {
+        public string Root { get; set; }
+        
+        public IList<FileElement> FileElements { get; set; }
+
+        public FileRepository() 
+        { 
+            Root = string.Empty;    
+            FileElements = new List<FileElement>();
+        }
+    }
+}
