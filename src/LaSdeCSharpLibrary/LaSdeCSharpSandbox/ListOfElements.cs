e24980afedde9ee7dd6b0214b41e13eff7b3636d
Christian Popescu <christian.popescu@outlook.com>
2025-03-20 23:55:39 +0100

    Add LaSDE CsharpConsole

---
+++ src/LaSdeCSharpLibrary/LaSdeCSharpSandbox/ListOfElements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaSdeCSharpSandbox
+{
+    // List of String elements
+
+    public class ListOfElements
+    {
+        public List<string> Elements { get; set; }
+
+        // Constructor to initialize the Elements list
+        public ListOfElements()
+        {
+            Elements = new List<string>();
+        }
+
+        public void LoadFromFileLines(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Elements.Add(line);
+                }
+            }
+
+        }
+    }
+}
---
+++ src/LaSdeCSharpLibrary/LaSdeCSharpSandbox/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaSdeCSharpSandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sandbox LaSdeCSharp!");
+        }
+    }
+}
