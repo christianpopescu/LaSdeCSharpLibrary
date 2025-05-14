abe28958f4d2d520274cb3fb9871bee8f8dc1079
Christian Popescu <christian.popescu@outlook.com>
2025-05-14 10:58:24 +0200

    Some refactorings and comments added

--- src/LaSdeCSharpLibrary/LaSdeCSharpWinForm/SimpleForms/FormActionLog.cs
+++ src/LaSdeCSharpLibrary/LaSdeCSharpWinForm/SimpleForms/FormActionLog.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LaSdeCSharpWinForm.SimpleForms
 {
     /// <summary>
     /// This class is used to do an action and log the evolution.
+    ///   Action<Action<string>> action is
     /// </summary>
     class FormActionLog : FormOkCancel
     {
         private System.Windows.Forms.TextBox textBoxLog = new TextBox();
         private System.Windows.Forms.Button buttonAction = new Button();
+        // action = a delegate to a function that does somenthing that takes a delegate as parameter that is used to log the evolution
         private Action<Action<string>> action = null;
         private void InitializeComponent(string actionButtonText, Action<Action<string>> actionForButton)
         {
@@ -51,7 +54,7 @@ namespace LaSdeCSharpWinForm.SimpleForms
         /// </summary>
         /// <param name="actionButtonText"> The text of the button </param>
         /// <param name="actionForButton"> Is the action that executed when the button is pressed
-        ///  the fuction takes one parameter a delegate used as callback for logging </param>
+        ///             the fuction takes one parameter a delegate used as callback for logging </param>
         public FormActionLog(string actionButtonText, Action<Action<string>> actionForButton  )
         {
             
