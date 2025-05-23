a92a645009093544d0f19a26224540d5736387a3
Christian Popescu <christian.popescu@outlook.com>
2025-05-23 22:30:52 +0200

    Add notes and documentation

--- src/LaSdeCSharpLibrary/SolutionDocumentation/LaSdeCSharpLibrary-main.md
+++ src/LaSdeCSharpLibrary/SolutionDocumentation/LaSdeCSharpLibrary-main.md
@@ -1,1 +1,3 @@
-﻿
+﻿# LaSdeCharLibrary Main Documentation 
+
+This is the entry point for the application documentation.
---
+++ src/LaSdeCSharpLibrary/SolutionDocumentation/Pages/NotesOnProject/src/ProgramWinForms.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace LaSdeCSharpWinForm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var form = new Form
+           {
+               Text = "Hello World",
+               Width = 800,
+               Height = 600
+           };
+            var label = new Label
+            {
+                Text = "Hello, World!",
+                Dock = DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+            };
+            form.Controls.Add(label);
+            Application.Run(form);
+        }
+    }
+}
